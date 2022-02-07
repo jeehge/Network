f9184a0320da5cc7983bfb936a6ad5901b1cf2c4
JH <taerg89@gmail.com>
2022-02-07 23:09:50 +0900

    Add New Type Enum

---
+++ NetworkStudy/NetworkStudy/Common/MenuCellType.swift
@@ -0,0 +1,17 @@
+//
+//  MenuCellType.swift
+//  NetworkStudy
+//
+//  Created by JH on 2022/02/07.
+//
+
+import Foundation
+
+// Cell Type 만들 때 공통으로 채택되면 좋을만한 정보를 추가한다
+protocol CellTypeProtocol {
+
+}
+
+enum MenuCellType: CellTypeProtocol {
+  case common
+}

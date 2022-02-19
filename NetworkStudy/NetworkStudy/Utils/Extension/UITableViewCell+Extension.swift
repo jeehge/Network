b66a5c2aa39621b11553f46dfc896891559254f0
JH <taerg89@gmail.com>
2022-02-20 00:32:22 +0900

    Add Extension Method

--- NetworkStudy/NetworkStudy/Utils/Extension/UITableViewCell+Extension.swift
+++ NetworkStudy/NetworkStudy/Utils/Extension/UITableViewCell+Extension.swift
@@ -15,7 +15,14 @@ extension UITableView {
     func registerNib<T: UITableViewCell>(_: T.Type) {
         let bundle: Bundle = Bundle(for: T.self)
         let nib: UINib = UINib(nibName: T.identifier, bundle: bundle)
-
         register(nib, forCellReuseIdentifier: T.identifier)
+    }
+    
+    func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
+        guard let cell = dequeueReusableCell(withIdentifier: T.identifier, for: indexPath) as? T else {
+            fatalError("Coluld not dequeue cell with identifier : \(T.identifier)")
+        }
+        
+        return cell
     }
 }

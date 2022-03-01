c6a08f3415aa22635862ad9623d886ace55e8b7f
JH <taerg89@gmail.com>
2022-03-01 10:22:49 +0900

    In SearchViewController.swift, replace the print statement in urlSession(_:downloadTask:didFinishDownloadingTo:) with the following code:

--- URLSession Materials/URLSession Starter/HalfTunes/View Controllers/SearchViewController.swift
+++ URLSession Materials/URLSession Starter/HalfTunes/View Controllers/SearchViewController.swift
@@ -237,6 +237,32 @@ extension SearchViewController: URLSessionDownloadDelegate {
   // 앱 다운로드가 끝나면 앱이 호출
   func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask,
                   didFinishDownloadingTo location: URL) {
-    print("Finished downloading to \(location).")
+    // 1
+    guard let sourceURL = downloadTask.originalRequest?.url else {
+      return
+    }
+
+    let download = downloadService.activeDownloads[sourceURL]
+    downloadService.activeDownloads[sourceURL] = nil
+    // 2
+    let destinationURL = localFilePath(for: sourceURL)
+    print(destinationURL)
+    // 3
+    let fileManager = FileManager.default
+    try? fileManager.removeItem(at: destinationURL)
+
+    do {
+      try fileManager.copyItem(at: location, to: destinationURL)
+      download?.track.downloaded = true
+    } catch let error {
+      print("Could not copy file to disk: \(error.localizedDescription)")
+    }
+    // 4
+    if let index = download?.track.index {
+      DispatchQueue.main.async { [weak self] in
+        self?.tableView.reloadRows(at: [IndexPath(row: index, section: 0)],
+                                   with: .none)
+      }
+    }
   }
 }

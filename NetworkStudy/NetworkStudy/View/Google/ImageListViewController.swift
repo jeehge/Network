ffa870c3df27c4a265772a71194293d6e47d454e
JH <taerg89@gmail.com>
2022-08-14 19:23:49 +0900

    작업중인거 임시 커밋

--- NetworkStudy/NetworkStudy/View/Google/ImageListViewController.swift
+++ NetworkStudy/NetworkStudy/View/Google/ImageListViewController.swift
@@ -11,9 +11,28 @@ import Foundation
  이미지 목록
  */
 final class ImageListViewController: BaseViewController {
-    
     // MARK: - Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+		
+//		getImageList(keyword: "test")
+		
+		let viewerVC = ImageViewerViewController.viewController(from: .google)
+		navigationController?.pushViewController(viewerVC, animated: true)
+    }
+
+    // MARK: - Method
+
+    private func getImageList(keyword: String) {
+		ImageService().getImageList(api: .image(keyword)){ [weak self] (result: Result<[ImageList], APIError>) in
+            guard let self = self else { return }
+            switch result {
+            case let .success(result):
+                print(result)
+            case let .failure(error):
+                print(error)
+            }
+        }
     }
 }
